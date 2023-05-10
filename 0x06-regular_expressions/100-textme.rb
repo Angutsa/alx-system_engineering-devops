7ca4b852beabfd604ae66417912d41c934a1c34f
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-05-10 21:36:20 +0300

    Add solution

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[\w\s\W]+?\[from:([\W\w]+)\][\w\s\W]+?\[to:([\w\+]+)[\w\W\s]+?\[flags:([-\d:]+)/).join
+puts ARGV[0].scan(/[\w\s\W]+?\[from:([\W\w]+)\][\w\s\W]+?\[to:([\w\+]+)[\w\W\s]+?\[flags:([-\d:]+)/).join(",")
